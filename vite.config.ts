9bb001a442311e7646e1c51f3bcc1f88a6de36c3
Gerard van der Stelt <g.g.vanderstelt@gmail.com>
2025-07-09 21:02:54 +0200

    8

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,6 @@ errorOnDuplicatesPkgDeps(devDependencies, dependencies);
 
 export default defineConfig(({ command, mode }): UserConfig => {
   return {
-    base: "/",
     plugins: [qwikCity(), qwikVite(), tsconfigPaths(), tailwindcss()],
     // This tells Vite which dependencies to pre-build in dev mode.
     optimizeDeps: {
