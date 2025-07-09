f4da74d0a97907c5ed3eb6808094f9c5a6eda771
Gerard van der Stelt <g.g.vanderstelt@gmail.com>
2025-07-09 21:30:12 +0200

    16

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -3,18 +3,18 @@ import { extendConfig } from "@builder.io/qwik-city/vite";
 import baseConfig from "../../vite.config";
 
 export default extendConfig(baseConfig, () => {
-  return {
-    build: {
-      ssr: true,
-      rollupOptions: {
-        input: ["@qwik-city-plan"],
-      },
-    },
-    base: "/vloer-expert",
-    plugins: [
-      staticAdapter({
-        origin: "https://gerardstelt.github.io"
-      }),
-    ],
-  };
+    return {
+        build: {
+            ssr: true,
+            rollupOptions: {
+                input: ["@qwik-city-plan"],
+            },
+        },
+        plugins: [
+            staticAdapter({
+                origin: "https://gerardstelt.github.io",
+                base: "/vloer-expert"
+            }),
+        ],
+    };
 });
